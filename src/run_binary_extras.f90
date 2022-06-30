4eb05f8dcbf6af98ce9f320f6182067d7ecc265e
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2022-06-30 15:30:08 +0200

    add comparison between power of eps_nuc y eps_neu for WD formation check

--- src/run_binary_extras.f90
+++ src/run_binary_extras.f90
@@ -744,23 +744,23 @@
 
          ! if star has too low-mass, assume WD formation, thus not important for us
          if (b% point_mass_i == 0) then
-            if (b% s1% c_core_mass < chandra_mass) then
+            if (b% s1% c_core_mass < chandra_mass .and. b% s1% power_nuc_burn < b% s1% power_neutrinos) then
                b% s1% termination_code = t_xtra1
                termination_code_str(t_xtra1) = 'white-dwarf'
                extras_binary_finish_step = terminate
-            else if (b% s2% c_core_mass < chandra_mass) then
+            else if (b% s2% c_core_mass < chandra_mass .and. b% s2% power_nuc_burn < b% s2% power_neutrinos) then
                b% s2% termination_code = t_xtra1
                termination_code_str(t_xtra1) = 'white-dwarf'
                extras_binary_finish_step = terminate
             end if
          else if (b% point_mass_i == 1) then
-            if (b% s2% c_core_mass < chandra_mass) then
+            if (b% s2% c_core_mass < chandra_mass .and. b% s2% power_nuc_burn < b% s2% power_neutrinos) then
                b% s2% termination_code = t_xtra1
                termination_code_str(t_xtra1) = 'white-dwarf'
                extras_binary_finish_step = terminate
             end if
          else if (b% point_mass_i == 2) then
-            if (b% s1% c_core_mass < chandra_mass) then
+            if (b% s1% c_core_mass < chandra_mass .and. b% s1% power_nuc_burn < b% s1% power_neutrinos) then
                b% s1% termination_code = t_xtra1
                termination_code_str(t_xtra1) = 'white-dwarf'
                extras_binary_finish_step = terminate
