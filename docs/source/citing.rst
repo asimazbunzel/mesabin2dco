1bc18ed82abe2c9d2fff13142125b3c027294cb8
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2023-04-28 14:06:48 -0300

    fix typo on bibtex file

--- docs/source/citing.rst
+++ docs/source/citing.rst
@@ -17,19 +17,19 @@ Here is the bibtex. Other options can be found in `ads <https://ui.adsabs.harvar
 ::
   
   @ARTICLE{garcia2021,
-       author = {{Garc{\'\i}a}, Federico and {Simaz Bunzel}, Adolfo and {Chaty}, Sylvain and {Porter}, Edward and {Chassande-Mottin}, Eric},
-        title = "{Progenitors of low-mass binary black-hole mergers in the isolated binary evolution scenario}",
-      journal = {\aap},
-     keywords = {gravitational waves, binaries: general, stars: evolution, stars: black holes, Astrophysics - High Energy Astrophysical Phenomena, Astrophysics - Solar and Stellar Astrophysics},
-         year = 2021,
-        month = may,
-       volume = {649},
-          eid = {A114},
-        pages = {A114},
-          doi = {10.1051/0004-6361/202038357},
-archivePrefix = {arXiv},
-       eprint = {2103.03161},
- primaryClass = {astro-ph.HE},
-       adsurl = {https://ui.adsabs.harvard.edu/abs/2021A&A...649A.114G},
-      adsnote = {Provided by the SAO/NASA Astrophysics Data System}
-}
+         author = {{Garc{\'\i}a}, Federico and {Simaz Bunzel}, Adolfo and {Chaty}, Sylvain and {Porter}, Edward and {Chassande-Mottin}, Eric},
+          title = "{Progenitors of low-mass binary black-hole mergers in the isolated binary evolution scenario}",
+        journal = {\aap},
+       keywords = {gravitational waves, binaries: general, stars: evolution, stars: black holes, Astrophysics - High Energy Astrophysical Phenomena, Astrophysics - Solar and Stellar Astrophysics},
+           year = 2021,
+          month = may,
+         volume = {649},
+            eid = {A114},
+          pages = {A114},
+            doi = {10.1051/0004-6361/202038357},
+  archivePrefix = {arXiv},
+         eprint = {2103.03161},
+   primaryClass = {astro-ph.HE},
+         adsurl = {https://ui.adsabs.harvard.edu/abs/2021A&A...649A.114G},
+        adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+  }
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -18,7 +18,9 @@
 # -- Project information -----------------------------------------------------
 
 project = 'MESAbin2dco'
-author = 'asimazbunzel & garciafederico on GitHub'
+author = 'asimazbunzel, garciafederico & Binary-rEvolution collab on GitHub'
+source_suffix = ".rst"
+master_doc = "index"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,47 +29,44 @@ author = 'asimazbunzel & garciafederico on GitHub'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autosectionlabel',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# -- Additional configuration ------------------------------------------------
-
-# sphinx_rtd options
-html_theme_options = {
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
-    'logo_only': True
-}
-
-# Set master doc
-master_doc = 'index'
-
-# Set logo
-# html_logo = ''
 
 # set default highlighting language
 highlight_language = 'fortran'
+
+pygments_style = "trac"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["."]
+
+extlinks = {
+    "issue": ("https://github.com/asimazbunzel/stevma/issues/%s", "#"),
+    "pr": ("https://github.com/asimazbunzel/stevma/pull/%s", "PR #"),
+}
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+
+if not on_rtd:  # only set the theme if we're building docs locally
+    html_theme = "sphinx_rtd_theme"
+
+html_use_smartypants = True
+html_last_updated_fmt = "%b %d, %Y"
+html_split_index = False
+html_sidebars = {
+    "**": ["searchbox.html", "globaltoc.html", "sourcelink.html"],
+}
+html_short_title = f"{project}-{version}"
+
+napoleon_use_ivar = True
+napoleon_use_rtype = False
+napoleon_use_param = False
